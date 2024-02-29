14f049442008b8bd78e5415769d04f54266d6da8
TheAndreiUrsu <u.andrei678@gmail.com>
2024-02-29 13:30:59 -0500

    Added sample code for navigation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Register from "./pages/register";
+import Questions from "./pages/questionairre";
+import Login from "./pages/sigin";
+import Profile from "./pages/user-profile";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Register />} />
+                <Route path="/questions" element={<Questions />} />
+                <Route
+                    path="/Login"
+                    element={<Login />}
+                />
+                <Route path="/Profile" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
 }
-
+ 
 export default App;
