2cca869bbf36abc8388a606740c35883ccb5158b
Andrei Ursu <u.andrei678@gmail.com>
2024-03-01 11:57:40 -0500

    Began working on register page.

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,7 +1,11 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 const register = () => {
     return (
         <div>
-            <h1>Register</h1>
+            <h1>Welcome new GreenCart User!</h1>
         </div>
     );
 }
