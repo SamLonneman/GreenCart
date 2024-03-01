d54269db71e3022a729120ec55f49e59996652c9
Sebastian Valdes <svaldes1@ufl.edu>
2024-02-29 20:01:20 -0500

    Even more changes! Prototype Sign In Page, alongside some new CSS styles.

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,6 +2,8 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <p1> Welcome to the home page</p1>
+            <p2> there really isn't much here</p2>
         </div>
     );
 }
