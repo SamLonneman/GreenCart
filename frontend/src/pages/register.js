84586b07b86148483a1460f1b55eee1fa6b416fa
Andrei Ursu <u.andrei678@gmail.com>
2024-03-01 12:28:05 -0500

    Added text fields.

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -2,11 +2,65 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-const register = () => {
-    return (
-        <div>
-            <h1>Welcome new GreenCart User!</h1>
-        </div>
-    );
+export default function Register(){
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
+    
+    return(
+        <Box class="center"
+            component = "form" 
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+              }}
+              noValidate
+              autoComplete="off"
+        >
+            <h1 class = "green "> Green<span class = "white">Cart</span> </h1>
+    <h2> Register Here </h2>
+            <div>
+                <TextField
+                    required
+                    id="standard-required"
+                    label="Email"
+                    variant="standard"
+                    sx={{ m: 1, width: '25ch' }}
+                />
+            </div>
+            <div>
+                <TextField
+                    required
+                    id="standard-required"
+                    label="Name"
+                    variant="standard"
+                    sx={{ m: 1, width: '25ch' }}
+                />
+            </div>
+            <div>
+                <TextField
+                    id="standard-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="standard"
+                    sx={{ m: 1, width: '25ch' }}
+                />
+            </div>
+            <div>
+                <TextField
+                    id="standard-password-input"
+                    label="Confirm Password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="standard"
+                    sx={{ m: 1, width: '25ch' }}
+                />
+            </div>
+
+        </Box>
+    )
 }
-export default register;
