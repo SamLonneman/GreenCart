b8efbb986dcd1f691c50555e99e6fc7e226ea1e7
Andrei Ursu <u.andrei678@gmail.com>
2024-03-01 14:39:01 -0500

    Added email, password, confirm password, and name textfields.

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import './pages.css';
+import { OutlinedInput } from '@mui/material';
 
 export default function Register(){
     const [showPassword, setShowPassword] = React.useState(false);
@@ -10,7 +17,7 @@ export default function Register(){
     const handleMouseDownPassword = (event) => {
       event.preventDefault();
     };
-    
+
     return(
         <Box class="center"
             component = "form" 
@@ -28,6 +35,7 @@ export default function Register(){
                     id="standard-required"
                     label="Email"
                     variant="standard"
+                    placeholder="albert@ufl.edu"
                     sx={{ m: 1, width: '25ch' }}
                 />
             </div>
@@ -37,19 +45,35 @@ export default function Register(){
                     id="standard-required"
                     label="Name"
                     variant="standard"
+                    placeholder="Albert"
                     sx={{ m: 1, width: '25ch' }}
                 />
             </div>
+            <FormControl x={{m: 1, width: '25ch' }} variant="standard">
             <div>
-                <TextField
-                    id="standard-password-input"
+                <OutlinedInput
+                    id="standard-adornment-password"
                     label="Password"
-                    type="password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff/> : <Visibility />}
+
+                            </IconButton>
+                        </InputAdornment>
+                    }
                     autoComplete="current-password"
                     variant="standard"
                     sx={{ m: 1, width: '25ch' }}
                 />
             </div>
+            </FormControl>
             <div>
                 <TextField
                     id="standard-password-input"
