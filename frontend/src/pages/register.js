4a448709a30c1b757867acc35ac6b6ed314e035c
TheAndreiUrsu <u.andrei678@gmail.com>
2024-03-05 17:19:01 -0500

    Added input fields for name, email, password, and confirm password. Added input validations but not using a regex to ensure the email address is using a valid domain. As well as password validations.
    TODO: Submit the input.

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,90 +1,76 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import './pages.css';
-import { OutlinedInput } from '@mui/material';
+import { useState } from 'react';
+import '../App.css';
+import { Container, Row, Button, Form, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default function Register(){
-    const [showPassword, setShowPassword] = React.useState(false);
+export default function Register() {
+  const [validated, setValidated] = useState(false);
 
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
-    const handleMouseDownPassword = (event) => {
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
       event.preventDefault();
-    };
+      event.stopPropagation();
+    }
 
-    return(
-        <Box class="center"
-            component = "form" 
-            sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-              }}
-              noValidate
-              autoComplete="off"
-        >
-            <h1 class = "green "> Green<span class = "white">Cart</span> </h1>
-    <h2> Register Here </h2>
-            <div>
-                <TextField
-                    required
-                    id="standard-required"
-                    label="Email"
-                    variant="standard"
-                    placeholder="albert@ufl.edu"
-                    sx={{ m: 1, width: '25ch' }}
-                />
-            </div>
-            <div>
-                <TextField
-                    required
-                    id="standard-required"
-                    label="Name"
-                    variant="standard"
-                    placeholder="Albert"
-                    sx={{ m: 1, width: '25ch' }}
-                />
-            </div>
-            <FormControl x={{m: 1, width: '25ch' }} variant="standard">
-            <div>
-                <OutlinedInput
-                    id="standard-adornment-password"
-                    label="Password"
-                    type={showPassword ? 'text' : 'password'}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {showPassword ? <VisibilityOff/> : <Visibility />}
+    setValidated(true);
+  };
 
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    autoComplete="current-password"
-                    variant="standard"
-                    sx={{ m: 1, width: '25ch' }}
-                />
-            </div>
-            </FormControl>
-            <div>
-                <TextField
-                    id="standard-password-input"
-                    label="Confirm Password"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="standard"
-                    sx={{ m: 1, width: '25ch' }}
-                />
-            </div>
 
-        </Box>
-    )
-}
+  return (
+    <div className="register">
+      <header className="register-header">
+        <Container>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row>
+              <Col>
+                <div>
+                  <Form.Group controlId="formEmail">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control required type="email" placeholder="gatoralbert@ufl.edu" />
+                    <Form.Control.Feedback type="invalid">Please enter your email.</Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <Form.Group controlId="formName">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control required type="text" placeholder="greencart_1" />
+                    <Form.Control.Feedback type="invalid">Please enter a username.</Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div>
+                  <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control required type="password" placeholder="" />
+                    <Form.Control.Feedback type="invalid">Please enter a password.</Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+              </Col>
+            </Row><Row>
+              <Col>
+                <div>
+                  <Form.Group controlId="confirmPassword">
+                    <Form.Label>Confirm password</Form.Label>
+                    <Form.Control required type="password" placeholder="" />
+                    <Form.Control.Feedback type="invalid">Please re-enter your password.</Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+              </Col>
+              <div>
+                <Button type="submit">Register</Button>
+              </div>
+            </Row>
+          </Form>
+        </Container>
+      </header>
+    </div>
+
+  )
+};
