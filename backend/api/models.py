37596ce39b413cf5fba38aeab1a1410531348672
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2024-03-12 15:23:50 -0500

    Filterable Table

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # NOTE: User model is already defined in django.contrib.auth.models
     
 class Products(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    category = models.IntegerField()
+    name = models.CharField(max_length=1000)
+    description = models.TextField(null=True, blank=True)
+    category = models.CharField(max_length=1000)
     sustainability_factor = models.IntegerField()
     image_link = models.CharField(max_length=10000)
--- frontend/src/pages/product-search.js
+++ frontend/src/pages/product-search.js
@@ -1,6 +1,79 @@
-import './pages.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+
+function FilterableProductTable({ products }) {
+    const [filterText, setFilterText] = useState('');
+
+    return (
+        <div>
+            <SearchBar 
+                filterText={filterText}
+                onFilterTextChange={setFilterText}/>
+            <ProductTable 
+                products={products} 
+                filterText={filterText}/>
+        </div>
+    );
+}
+
+function ProductRow({ product }) {
+    return (
+        <tr>
+            <td className="tablecell">{product.id}</td>
+            <td className="tablecell">{product.name}</td>
+            <td className="tablecell">{product.description}</td>
+            <td className="tablecell">{product.category}</td>
+            <td className="tablecell">{product.sustainability_factor}</td>
+            <td className="tablecell"><img src={product.image_link} alt={product.name} width="100" /></td>
+        </tr>
+    );
+}
+
+function ProductTable({ products, filterText }) {
+    const rows = [];
+    products.forEach((product) => {
+        if (
+            product.name.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) !== -1
+        ) {
+            rows.push(
+                <ProductRow
+                    product={product}
+                    key={product.name} />
+            );
+        }
+    });
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="tablecell">ID</th>
+                    <th className="tablecell">Name</th>
+                    <th className="tablecell">Description</th>
+                    <th className="tablecell">Category</th>
+                    <th className="tablecell">Sustainability Factor</th>
+                    <th className="tablecell">Image</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function SearchBar({
+    filterText,
+    onFilterTextChange,
+}) {
+    return (
+        <form>
+            <input 
+                type="text" 
+                value={filterText} placeholder="Search..." 
+                onChange={(e) => onFilterTextChange(e.target.value)} />
+        </form>
+    );
+}
 
 export default function ProductSearch() {
     const [products, setProducts] = useState([]);
@@ -11,35 +84,5 @@ export default function ProductSearch() {
         };
         fetchData();
     }, []);
-    const columns = ['ID', 'Name', 'Description', 'Category', 'Sustainability Factor', 'Image'];
-    const productProperties = ['id', 'name', 'description', 'category', 'sustainability_factor'];
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        {columns.map((column) => (
-                            <th key={column} className="tablecell">
-                                {column}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map((product) => (
-                        <tr key={product.id}>
-                            {productProperties.map((property) => (
-                                <td className="tablecell" key={property}>
-                                    {product[property]}
-                                </td>
-                            ))}
-                            <td className="tablecell">
-                                <img src={product.image_link} alt={product.name} width="100" />
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+    return <FilterableProductTable products={products} />;
 }
