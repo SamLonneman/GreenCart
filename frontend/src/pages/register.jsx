fc11b316b5aa8e36994a6c9a21fecf20d8ac3299
Andrei Ursu <u.andrei678@gmail.com>
2024-03-15 15:03:00 -0400

    Restructured the schema.

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -3,22 +3,44 @@ import { Link } from 'react-router-dom'
 import '../App.css';
 import { Card, Container, Row, Button, Form, Col } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-import { object, string, ref } from 'yup';
+import * as Yup from 'yup';
 import { yupResolver } from "@hookform/resolvers/yup";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+export const PASSWORD_REGEX = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&_]{8,20}$/);
+
+const messages = {
+  missingEmail: "Please enter an email address",
+  missingUsername: "Please enter a username.",
+  invalidPassword: "Password is not strong enough.",
+  noMatchPassword: "Passwords must match."
+};
+
+// Yup schema for register page.
+const RegisterSchema = Yup.object().shape({
+  email: Yup
+    .string()
+    .email('Invalid Email Address.')
+    .required(messages.missingEmail),
+  
+  username: Yup
+    .string()
+    .required(messages.missingUsername)
+    .max(16, "Username is too long."),
+  
+  password: Yup
+    .string()
+    .required("Please enter a password.")
+    .matches(PASSWORD_REGEX, messages.invalidPassword)
+    .min(8, "Password is too short, should be at least 8 characters."),
+
+  confirmPassword: Yup
+    .string()
+    //.oneOf([Yup.ref('password'), null], messages.noMatchPassword)
+});
+
 export default function Register() {
-
-  // Yup schema for register page.
-  const RegisterSchema = object().shape({
-    email: string().label('Email Address').email('Invalid Email Address').required("Please enter an email address."),
-
-    username: string().label('Username').required("Username is required.").max(16, "Username is too long."),
-
-    password: string().label('Password').required("Please enter a password.").min(8, "Password is too short, should be at least 8 characters."),
-
-    confirmPassword: string().label('Confirm Password').oneOf([ref('password'), null], "Passwords must match.")
-  });
 
   const {
     register,
@@ -28,13 +50,14 @@ export default function Register() {
     resolver: yupResolver(RegisterSchema)
   });
 
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => console.log(data);
 
   return (
     <div className="register">
       <header className="register-header">
         <Container>
           <Card>
+            <Card.Header>Create a new user</Card.Header><Card.Body>
             <Form onSubmit={handleSubmit((data) => onSubmit(data))}>
             <Row>
               <Col>
@@ -42,17 +65,67 @@ export default function Register() {
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control 
                     required type="email" 
-                    placeholder="gatoralbert@ufl.edu"
+                    placeholder="name@example.com"
                     isInvalid={errors.email}
                     {...register('email')}
                      />
                     <Form.Control.Feedback type="invalid">
                       {errors.email?.message}
                     </Form.Control.Feedback>
-                  </Form.Group>
+              </Form.Group>
+              </Col>
+              <Col>
+              <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                    required type="username" 
+                    placeholder="name"
+                    isInvalid={errors.username}
+                    {...register('username')}
+                     />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.username?.message}
+                    </Form.Control.Feedback>
+              </Form.Group>
               </Col>
             </Row>
+            <Row>
+              <Col>
+              <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                    required type="password" 
+                    placeholder="********"
+                    isInvalid={errors.password}
+                    {...register('password')}
+                     />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.password?.message}
+                    </Form.Control.Feedback>
+              </Form.Group>
+              </Col>
+              <Col>
+              <Form.Group controlId="formConfirmPass">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control 
+                    required type="password" 
+                    placeholder="********"
+                    isInvalid={errors.confirmPassword}
+                    {...register('confirmPassword')}
+                     />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.confirmPassword?.message}
+                    </Form.Control.Feedback>
+              </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <div>
+                <Button type="submit">Register</Button>
+              </div>
+            </Row>
             </Form>
+            </Card.Body>
           </Card>
           {/* <Form>
             <Card>
