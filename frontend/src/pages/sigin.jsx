a9c0c4a9512f1b929dd05b2d0518faf86470589f
TheAndreiUrsu <u.andrei678@gmail.com>
2024-03-22 10:07:31 -0400

    Added sign.jsx, and began working on react-hook-form and yup for the login page.

---
+++ frontend/src/pages/sigin.jsx
@@ -0,0 +1,249 @@
+// import React from 'react';
+// import IconButton from '@mui/material/IconButton';
+
+// import Input from '@mui/material/Input';
+// import InputLabel from '@mui/material/InputLabel';
+// import InputAdornment from '@mui/material/InputAdornment';
+
+// import FormControl from '@mui/material/FormControl';
+// import TextField from '@mui/material/TextField';
+// import Visibility from '@mui/icons-material/Visibility';
+// import VisibilityOff from '@mui/icons-material/VisibilityOff';
+// import { Box, Fab, FormHelperText } from '@mui/material';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Button, Form, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import './pages.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { green, grey} from '@mui/material/colors';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const theme = createTheme({
+  palette: {
+    primary: green,
+    secondary: grey,
+  },
+});
+const customTheme = (outerTheme) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            '--TextField-brandBorderColor': '#E0E3E7',
+            '--TextField-brandBorderHoverColor': '#B2BAC2',
+            '--TextField-brandBorderFocusedColor': '#6F7E8C',
+            '& label.Mui-focused': {
+              color: 'var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+      MuiInput: {
+        styleOverrides: {
+          root: {
+            '&::before': {
+              borderBottom: '2px solid var(--TextField-brandBorderColor)',
+            },
+            '&:hover:not(.Mui-disabled, .Mui-error):before': {
+              borderBottom: '2px solid var(--TextField-brandBorderHoverColor)',
+            },
+            '&.Mui-focused:after': {
+              borderBottom: '2px solid var(--TextField-brandBorderFocusedColor)',
+            },
+            '&MuiInputBase-root': {
+              color: 'white',
+            },
+          },
+        },
+      },
+      MuiFormControl: {
+        styleOverrides: {
+          root: {
+            '--TextField-brandBorderColor': '#E0E3E7',
+            '--TextField-brandBorderHoverColor': '#B2BAC2',
+            '--TextField-brandBorderFocusedColor': '#6F7E8C',
+            '& label.Mui-focused': {
+              color: 'var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+    },
+  });
+
+const messages = {
+  missingEmail: "Please enter an email address.",
+  missingUsername: "Please enter a username.",
+  missingPassword: "Please enter a password.",
+  invalidLogin: "Could not login, account does not exist or password is invalid."
+};
+
+// Yup schema for login page
+const LoginSchema = Yup.object().shape({
+  username: Yup
+    .string()
+    .required(messages.missingUsername),
+  
+  password: Yup
+    .string()
+    .required()
+})
+
+export default function Signin(){
+  
+  const {
+    signin,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(LoginSchema)
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  return(
+    <div className="login">
+      <header className="login-header">
+        <Container>
+          <h1 className='green'>Login</h1>
+          <Form onSubmit={handleSubmit((data) => onSubmit(data))}>
+            <Container>
+              <Row>
+                <Col>
+                  <Form.Group controlId="formUsername">
+                    <Form.Control
+                      className="custom-input"
+                      required type="username"
+                      placeholder="Name"
+                      isInvalid={errors.username}
+                      {...signin('username')}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.username?.message}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Col>
+              </Row>
+            </Container>
+            <Container>
+              <Row>
+                <Form.Group controlId="formPassword">
+                  <Form.Control
+                    className="custom-input"
+                    required type="password"
+                    placeholder="Password"
+                    isInvalid={errors.password}
+                    {...signin('password')}
+                  />
+                </Form.Group>
+              </Row>
+            </Container>
+          </Form>
+        </Container>
+      </header>
+    </div>
+  )
+};
+
+// export default function Sigin() {
+   
+//     const outerTheme = createTheme();
+//     const passRegex = new RegExp(/^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/);
+//     const [showPassword, setShowPassword] = React.useState(false);
+//     const [formData, setFormData] = React.useState({userquery: "", passquery: ""});
+//     const [isFormInvalid, setIsFormInvalid] = React.useState(false);
+//     const validate = values => {
+//       //alert(passRegex.test(values.passquery));  
+//       if (passRegex.test(values.passquery)) {
+//         setIsFormInvalid(false);
+//         return true;
+//       } else { 
+//         setIsFormInvalid(true);
+//         return false;
+//       }
+//     };
+//     function handleFormChange(event) {
+//       let data = formData;
+//       data[event.target.name] = event.target.value;
+//       setFormData(data);
+//     }
+//     const handleSubmit = (event) => {
+//       event.preventDefault();
+//       if (validate(formData)) {
+//         alert("Form is valid");
+//         // submission logic
+//       } 
+//     }
+//     const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+//     const handleMouseDownPassword = (event) => {
+//       event.preventDefault();
+//     };
+//     function login(formData){
+//       const query = formData.get('userquery');
+//       alert(query);
+//       const pass = formData.get('passquery');
+//       alert(pass);
+//     }
+
+//     return (<Box class = "center grey">
+//     <h1 class = "green">Green<span>Cart</span> </h1>
+//     <h2 class ="largetext"> Please Log In</h2>
+//     <form onSubmit={handleSubmit}>
+//     <ThemeProvider theme={customTheme(outerTheme)}>
+//     <div>
+//          <TextField required userquery InputLabelProps={{sx: {color: 'grey'},}} InputProps={{style: {color: 'grey'},}} sx={{ marginLeft: 1, width: '25ch', marginTop: 0}}  id="standard-basic" label="Username" variant="standard" margin = "dense" />
+//     </div>
+//     <div>
+//     <FormControl sx={{ marginLeft: 1, width: '25ch'}} variant="standard" required   error = {isFormInvalid}    onChange = {handleFormChange}  >
+//           <InputLabel htmlFor="standard-adornment-password" sx = {{color: 'grey'}} >Password</InputLabel >
+//           <Input
+//             name = "passquery"
+//             helperText = {isFormInvalid ? "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character" : ""}
+//             id="standard-adornment-password"
+//             type={showPassword ? 'text' : 'password'}
+//             sx = {{color: 'grey'}}
+//             endAdornment={
+//               <InputAdornment position="end">
+//                 <IconButton
+//                   aria-label="toggle password visibility"
+//                   onClick={handleClickShowPassword}
+//                   onMouseDown={handleMouseDownPassword}
+//                   edge="end"
+//                   sx = {{color: 'white'}}
+//                 >
+//                   {showPassword ? <VisibilityOff /> : <Visibility />}
+//                 </IconButton>
+//               </InputAdornment>
+//             }
+//             label="Password"
+//           />
+//           {isFormInvalid ? <FormHelperText id="standard-weight-helper-text">Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character</FormHelperText> : ""}
+//         </FormControl>
+//     </div>
+//     </ThemeProvider>
+//     <div>
+//     <div>
+//     <p class = "minimummargin"><a href = "/forgotpassword" class = "green">Forgot Password?</a></p>
+//     </div>
+      
+//     <Fab sx={{ m: 1, width: '27.5ch' }} variant="extended" color="secondary" theme = {theme}
+//       type = "submit">
+//         Sign In
+//     </Fab>
+//     </div>
+//     </form>
+//     <div>
+//     <p class = "smalltext">Don't have an account? <a href = "/register" class = "green">Sign Up</a></p>
+//     </div>
+//     </Box>
+
+//     );
+// };
