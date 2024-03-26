8625f547d628fcf847d610cd4293afe193881bb5
TheAndreiUrsu <u.andrei678@gmail.com>
2024-03-26 11:06:57 -0400

    Working on adding icons to register page.

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 import '../App.css';
 import { Container, Row, Button, Form, Col } from 'react-bootstrap';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -91,6 +92,10 @@ const Register = ({ register, isAuthenticated }) => {
                 <Row className="justify-content-center">
                   <Col>
                     <Form.Group controlId="formUsername">
+                      <InputGroup>
+                        <InputGroup.Text>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                        </InputGroup.Text>
                       <Form.Control
                         className="custom-input"
                         required type="username"
@@ -100,6 +105,7 @@ const Register = ({ register, isAuthenticated }) => {
                         value={username}
                         isInvalid={errors.username}
                       />
+                      </InputGroup>
                       <Form.Control.Feedback type="invalid">
                         {errors.username?.message}
                       </Form.Control.Feedback>
