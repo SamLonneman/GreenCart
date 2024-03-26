6b9e94bdacf03f2812fedbcac4d72d9ec97bbab7
TheAndreiUrsu <u.andrei678@gmail.com>
2024-03-26 14:32:00 -0400

    Added a footer component.

---
+++ frontend/src/pages/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <aside>
+                <p>GreenCart LLC</p>
+            </aside>
+        </footer>
+    );
+};
+
+export default Footer;
