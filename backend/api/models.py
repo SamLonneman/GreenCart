81372ae15c6e2d4cebf28f85aef6be857fcac5e3
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2024-04-06 18:23:13 -0400

    Created task model and views

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
     
 class Products(models.Model):
@@ -7,3 +8,17 @@ class Products(models.Model):
     sustainability_factor = models.FloatField()
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     image_link = models.CharField(max_length=10000)
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000)
+    description = models.TextField(null=True, blank=True)
+    expected_time_commitment = models.IntegerField(default=30)
+    is_challenging = models.BooleanField(default=False)
+    is_community_oriented = models.BooleanField(default=False)
+    is_impactful = models.BooleanField(default=False)
+    is_learning_task = models.BooleanField(default=False)
+    time_accepted = models.DateTimeField(auto_now_add=True)
+    time_completed = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    time_completed = models.DateTimeField(null=True, blank=True)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ProductList
+from .views import GetTasksView, CreateTaskView, CompleteTaskView, DeleteTaskView, ProductList
 
 urlpatterns = [
-    path('products', ProductList.as_view())
+    path('products', ProductList.as_view()),
+    path('tasks', GetTasksView.as_view()),
+    path('tasks/create', CreateTaskView.as_view()),
+    path('tasks/complete', CompleteTaskView.as_view()),
+    path('tasks/delete', DeleteTaskView.as_view())
 ]
