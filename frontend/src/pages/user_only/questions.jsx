888894ae19cd61b62a29535081581c20d3b40d2e
TheAndreiUrsu <u.andrei678@gmail.com>
2024-04-08 14:14:09 -0400

    Started questions page.

---
+++ frontend/src/pages/user_only/questions.jsx
@@ -0,0 +1,17 @@
+import CSRFToken from "../../components/CSRFToken";
+import Cart from '../../icons/cart.png';
+import { Navigate } from 'react-router-dom';
+import React from 'react';
+
+const Questions = () => {
+
+    return(
+        <div className="center">
+            <div className="center">
+                
+            </div>
+        </div>
+    )
+};
+
+export default Questions;
