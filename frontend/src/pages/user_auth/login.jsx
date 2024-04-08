6ab5547de9b000b16402aa02e6eb086d3307e6c0
TheAndreiUrsu <u.andrei678@gmail.com>
2024-04-08 14:14:23 -0400

    Removed useState import.

--- frontend/src/pages/user_auth/login.jsx
+++ frontend/src/pages/user_auth/login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
