6eae0cbab60a93d00722bc5c93684be109e70e07
TheAndreiUrsu <u.andrei678@gmail.com>
2024-04-08 22:23:19 -0400

    Added a countdown for app release.

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,10 +1,15 @@
+import CountdownTimer from "./countdown";
+
 const Home = () => {
     return (
-        <div>
+        <div className="center">
             <h1>Home</h1>
             <p1> Welcome to the home page</p1>
             <p2> there really isn't much here</p2>
+            <h1> Greencart will be released in...</h1>
+            <CountdownTimer targetDate={new Date("April 19, 2024 00:00:00").getTime()} />
         </div>
+
     );
 }
 export default Home;
