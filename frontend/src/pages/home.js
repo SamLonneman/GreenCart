5ab2047779d6924b14f36361acb648dec306d281
TheAndreiUrsu <u.andrei678@gmail.com>
2024-04-08 22:26:35 -0400

    Updated the time

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,11 +3,8 @@ import CountdownTimer from "./countdown";
 const Home = () => {
     return (
         <div className="center">
-            <h1>Home</h1>
-            <p1> Welcome to the home page</p1>
-            <p2> there really isn't much here</p2>
             <h1> Greencart will be released in...</h1>
-            <CountdownTimer targetDate={new Date("April 19, 2024 00:00:00").getTime()} />
+            <CountdownTimer targetDate={new Date("April 24, 2024 15:00:00").getTime()} />
         </div>
 
     );
