c8df46c4450e17b41f8595c63e813daa9eae71c6
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2024-04-09 22:32:45 -0400

    Added generate-task to AIAPI

--- backend/AIapi/urls.py
+++ backend/AIapi/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GenerateTextView
+from .views import GenerateTextView, GenerateTaskView
 # ai time
 urlpatterns = [
     path('request', GenerateTextView.as_view()),
+    path('generate-task', GenerateTaskView.as_view()),
 ]
--- backend/AIapi/views.py
+++ backend/AIapi/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 import json
 from django.http import JsonResponse
 import google.generativeai as genai
+
+from api.models import Task
+from api.serializers import TaskSerializer
 from .models import AIapi
 import os
 import environ
+
+
 env = environ.Env()
 environ.Env.read_env()
 # Create your views here.
@@ -23,4 +31,20 @@ class GenerateTextView(APIView):
             #user = request.user
             #AIapi.objects.create(user=user, text_input=text, generated_text=response.text)
             return Response({'generated_text': response.text})
-        
+
+@permission_classes([IsAuthenticated])
+class GenerateTaskView(APIView):
+    def get(self, request, format=None):
+        task = Task()
+        task.user = self.request.user
+        # TODO: REPLACE SAMPLE BELOW WITH AI GENERATED DATA
+        task.title = 'AI Generated Title'
+        task.description = 'AI generated description...'
+        task.expected_time_commitment = 15
+        task.is_challenging = True
+        task.is_community_oriented = False
+        task.is_impactful = True
+        task.is_learning_task = False
+        # You may also override task.due_date if you want, otherwise it defaults to a week from now
+        task.save()
+        return Response({'task': TaskSerializer(task).data}, status=status.HTTP_201_CREATED)
