f1a5398b370c5b416053e0ce64468177647ab8fe
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2024-04-17 12:49:28 -0400

    Added dates to cards

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -36,17 +36,30 @@ const TaskCard = ({ task, showAcceptButton, showRejectButton, showCompleteButton
         getCompletedTasks();
     };
 
+    let dateText = null;
+    if (task.is_accepted && !task.is_completed)
+        dateText = `Due: ${new Date(task.due_date).toLocaleDateString()}`;
+    else if (task.is_completed)
+        dateText = `Completed: ${new Date(task.completed_date).toLocaleDateString()}`;
+
     return (
         <Card sx={{ width: '300px', borderRadius: '16px', margin: '10px auto', backgroundColor: '#f5f5f5', boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.25)' }}>
             <CardContent>
-                <Typography variant="h5" component="div">{task.title}</Typography>
-                <Typography variant="body2" sx={{ marginBottom: '10px' }}>{task.description}</Typography>
-                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Typography variant="h5" component="div">
+                    {task.title}
+                </Typography>
+                <Typography variant="body2" sx={{ marginBottom: '10px' }}>
+                    {task.description}
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mb: '10px'}}>
                     {showAcceptButton && <Button variant="contained" color="success" onClick={handleAccept}>Accept</Button>}
                     {showRejectButton && <Button variant="contained" color="error" onClick={handleReject}>Reject</Button>}
                     {showCompleteButton && <Button variant="contained" color="success" onClick={handleComplete}>Complete</Button>}
                     {showDeleteButton && <Button variant="contained" color="error" onClick={handleDelete}>Delete</Button>}
                 </Box>
+                {dateText && <Typography variant="body2" color="text.secondary" align="right">
+                    {dateText}
+                </Typography>}
             </CardContent>
         </Card>
     );
