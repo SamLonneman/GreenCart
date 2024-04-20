64fec0d4dde2a08c97f0eb4e56d3848b7f3c6985
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2024-04-19 22:57:46 -0400

    Task stats endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from django.utils import timezone
 
+from django.db.models import Avg, F
+from django.db.models.functions import Cast
+from django.db.models.fields import DurationField
+
 from rest_framework import status
 from rest_framework.decorators import permission_classes
 from rest_framework.generics import ListAPIView
@@ -64,6 +68,37 @@ class GetAllTasksView(APIView):
         tasks = user.task_set.filter(is_accepted=True).order_by('-accepted_date')
         tasks = TaskSerializer(tasks, many=True)
         return Response({'tasks': tasks.data})
+
+# Get task statistics for progress tracking
+@permission_classes([IsAuthenticated])
+class GetTaskStatsView(APIView):
+    def get(self, request, format=None):
+        user = self.request.user
+        tasks = user.task_set.all()
+        completedTasks = tasks.filter(is_completed=True)
+        num_tasks_accepted = tasks.filter(is_accepted=True).count()
+        num_tasks_completed = completedTasks.count()
+        average_turnaround_time = completedTasks.aggregate(
+            avg_turnaround=Avg(
+                Cast(F('completed_date') - F('accepted_date'), DurationField())
+            )
+        )['avg_turnaround']
+        average_turnaround_time = average_turnaround_time.total_seconds() / 86400 if average_turnaround_time else None
+        num_community_oriented = completedTasks.filter(is_community_oriented=True).count()
+        num_learning = completedTasks.filter(is_learning_task=True).count()
+        num_impactful = completedTasks.filter(is_impactful=True).count()
+        num_challenging = completedTasks.filter(is_challenging=True).count()
+        return Response({
+            "stats": {
+                "num_tasks_accepted": num_tasks_accepted,
+                "num_tasks_completed": num_tasks_completed,
+                "average_turnaround_time": average_turnaround_time,
+                "num_community_oriented": num_community_oriented,
+                "num_learning": num_learning,
+                "num_impactful": num_impactful,
+                "num_challenging": num_challenging
+            }
+        })
 
 # Accept a task suggested by the AI
 @permission_classes([IsAuthenticated])
