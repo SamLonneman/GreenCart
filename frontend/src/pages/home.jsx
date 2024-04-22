d7ec6d2dd181e2908c332f3e5a37619a209cae3f
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2024-04-22 17:04:15 -0400

    Created home page

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CSRFToken from './../components/CSRFToken';
+import Cart from './../icons/cart.png';
+import './pages.css';
+
+const Home = () => {
+  return (
+    <div className="signin-form">
+      <div className="center">
+        <div className="custom-box center border-solid">
+          <form>
+            <div></div>
+            <CSRFToken />
+            <img src={Cart} alt="GreenCart"/>
+            <h1 className="center-text">GreenCart</h1>
+            <h2>Reimagining sustainability, one goal at a time.</h2>
+            <h4>Please log in or sign up to get started.</h4>
+            <div className="form-group">
+              <Link to="/login">
+                <button className="custom-text btn button">Log In</button>
+              </Link>
+            </div>
+            <p className="custom-text">or</p>
+            <div className="form-group">
+              <Link to="/register">
+                <button className="custom-text btn button">Sign up</button>
+              </Link>
+            </div>
+            <div></div>
+            <div></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
